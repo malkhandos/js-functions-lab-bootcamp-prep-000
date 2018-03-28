74b0f5a3a78d3e2c06a3b72d95c56abab921728d
Rommy Elyan <relyan@email.arizona.edu>
2018-03-28 18:24:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function happyHolidays(){
   return 'Happy holidays!'
 }
 function happyHolidaysTo(name){
-  return `Happy Holidays, ${name}!`
+  return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name){
-  return `Happy ${holidays}, ${name}!`
+  return `Happy ${holiday}, ${name}!`
 }
-function happyCountdown(days, holiday){
+function holidayCountdown(days, holiday){
   return `It's ${days} until ${holiday}!`
 }
